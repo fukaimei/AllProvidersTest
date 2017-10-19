db93d06dd2ca0bf58c6f74b9805a47efd29987b6
傅开煤 <fukaimei@aliyun.com>
2017-10-20 00:30:05 +0800

    Initial commit

---
+++ app/src/main/java/com/fukaimei/allproviderstest/MainActivity.java
@@ -0,0 +1,45 @@
+package com.fukaimei.allproviderstest;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView providers;
+    LocationManager lm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        providers = (ListView) findViewById(R.id.providers);
+        // 获取系统的LocationManager对象
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // 获取系统所有的LocationProvider的名称
+        List<String> providerNames = lm.getAllProviders();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, providerNames);
+        // 使用ListView来显示所有可用的LocationProvider
+        providers.setAdapter(adapter);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
